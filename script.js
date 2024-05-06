cc939b7236585d974d357a4aea8403c54816fb54
Artur Nikytenko <arturnikytenko@gmail.com>
2024-05-06 12:48:33 +0200

    Adding popup

--- script.js
+++ script.js
@@ -23,9 +23,23 @@ if (navigator.geolocation) {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker(coords).addTo(map)
-            .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-            .openPopup();
+        map.on('click', function (mapEvent) {
+            const { lat, lng } = mapEvent.latlng
+
+            L.marker([lat, lng])
+                .addTo(map)
+                .bindPopup(
+                    L.popup({
+                        maxWidth: 250,
+                        minWifth: 100,
+                        autoClose: false,
+                        closeOnClick: false,
+                        className: 'running-popup',
+                    })
+                )
+                .setPopupContent('Workout')
+                .openPopup();
+        });
     }, function () {
         alert('Could not get your position');
     });
